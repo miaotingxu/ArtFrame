bbcbde6a0fa530851aec515c8e3fc72cd92738df
miaotingxu <jiayuzhen@haier.com>
2020-01-05 12:30:46 +0800

    first commit

---
+++ app/src/main/java/art/com/artdemo1/mvp/ui/activity/UserActivity.java
@@ -0,0 +1,151 @@
+package art.com.artdemo1.mvp.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.paginate.Paginate;
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import java.util.ArrayList;
+
+import art.com.artdemo1.R;
+import art.com.artdemo1.mvp.presenter.UserPresenter;
+import art.com.artdemo1.mvp.ui.adapter.UserAdapter;
+import butterknife.BindView;
+import me.jessyan.art.base.BaseActivity;
+import me.jessyan.art.base.DefaultAdapter;
+import me.jessyan.art.mvp.IView;
+import me.jessyan.art.mvp.Message;
+import me.jessyan.art.utils.ArtUtils;
+
+import static me.jessyan.art.utils.Preconditions.checkNotNull;
+
+/**
+ * ================================================
+ * 展示 View 的用法
+ * <p>
+ * Created by JessYan on 09/04/2016 10:59
+ * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
+ * <a href="https://github.com/JessYanCoding">Follow me</a>
+ * ================================================
+ */
+public class UserActivity extends BaseActivity<UserPresenter> implements IView, SwipeRefreshLayout.OnRefreshListener {
+
+    @BindView(R.id.recyclerView)
+    RecyclerView mRecyclerView;
+    @BindView(R.id.swipeRefreshLayout)
+    SwipeRefreshLayout mSwipeRefreshLayout;
+
+    private Paginate mPaginate;
+    private boolean isLoadingMore;
+    private RxPermissions mRxPermissions;
+    private UserAdapter mAdapter;
+
+
+    @Override
+    public int initView(@Nullable Bundle savedInstanceState) {
+        return R.layout.activity_user;
+    }
+
+    @Override
+    public void initData(@Nullable Bundle savedInstanceState) {
+        initRecyclerView();
+        mRecyclerView.setAdapter(mAdapter);
+        initPaginate();
+        mPresenter.requestUsers(Message.obtain(this, new Object[]{true}));//打开app时自动加载列表
+    }
+
+    @Override
+    @Nullable
+    public UserPresenter obtainPresenter() {
+        this.mRxPermissions = new RxPermissions(this);
+        this.mAdapter = new UserAdapter(new ArrayList<>());
+        return new UserPresenter(ArtUtils.obtainAppComponentFromContext(this), mAdapter, mRxPermissions);
+    }
+
+    @Override
+    public void showLoading() {
+        mSwipeRefreshLayout.setRefreshing(true);
+    }
+
+    @Override
+    public void hideLoading() {
+        mSwipeRefreshLayout.setRefreshing(false);
+    }
+
+    @Override
+    public void showMessage(@NonNull String message) {
+        checkNotNull(message);
+        ArtUtils.snackbarText(message);
+    }
+
+    @Override
+    public void handleMessage(@NonNull Message message) {
+        checkNotNull(message);
+        switch (message.what) {
+            case 0:
+                isLoadingMore = true;//开始加载更多
+                break;
+            case 1:
+                isLoadingMore = false;//结束加载更多
+                break;
+        }
+    }
+
+
+    @Override
+    public void onRefresh() {
+        mPresenter.requestUsers(Message.obtain(this, new Object[]{true}));
+    }
+
+    /**
+     * 初始化RecyclerView
+     */
+    private void initRecyclerView() {
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+        ArtUtils.configRecyclerView(mRecyclerView, new GridLayoutManager(this, 2));
+    }
+
+
+    /**
+     * 初始化Paginate,用于加载更多
+     */
+    private void initPaginate() {
+        if (mPaginate == null) {
+            Paginate.Callbacks callbacks = new Paginate.Callbacks() {
+                @Override
+                public void onLoadMore() {
+                    mPresenter.requestUsers(Message.obtain(UserActivity.this, new Object[]{false}));
+                }
+
+                @Override
+                public boolean isLoading() {
+                    return isLoadingMore;
+                }
+
+                @Override
+                public boolean hasLoadedAllItems() {
+                    return false;
+                }
+            };
+
+            mPaginate = Paginate.with(mRecyclerView, callbacks)
+                    .setLoadingTriggerThreshold(0)
+                    .build();
+            mPaginate.setHasMoreDataToLoad(false);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        DefaultAdapter.releaseAllHolder(mRecyclerView);//super.onDestroy()之后会unbind,所有view被置为null,所以必须在之前调用
+        super.onDestroy();
+        this.mRxPermissions = null;
+        this.mPaginate = null;
+    }
+}
---
+++ app/src/main/java/art/com/artdemo1/mvp/ui/holder/UserItemHolder.java
@@ -0,0 +1,77 @@
+package art.com.artdemo1.mvp.ui.holder;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import art.com.artdemo1.R;
+import art.com.artdemo1.mvp.model.entity.User;
+import butterknife.BindView;
+import me.jessyan.art.base.BaseHolder;
+import me.jessyan.art.base.DefaultAdapter;
+import me.jessyan.art.di.component.AppComponent;
+import me.jessyan.art.http.imageloader.ImageLoader;
+import me.jessyan.art.http.imageloader.glide.ImageConfigImpl;
+import me.jessyan.art.utils.ArtUtils;
+
+/**
+ * ================================================
+ * 展示 {@link BaseHolder} 的用法
+ * <p>
+ * Created by JessYan on 9/4/16 12:56
+ * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
+ * <a href="https://github.com/JessYanCoding">Follow me</a>
+ * ================================================
+ */
+public class UserItemHolder extends BaseHolder<User> {
+
+    @BindView(R.id.iv_avatar)
+    ImageView mAvatar;
+    @BindView(R.id.tv_name)
+    TextView mName;
+
+    private AppComponent mAppComponent;
+    /**
+     * 用于加载图片的管理类, 默认使用 Glide, 使用策略模式, 可替换框架
+     */
+    private ImageLoader mImageLoader;
+
+    public UserItemHolder(View itemView) {
+        super(itemView);
+        //可以在任何可以拿到Application的地方, 拿到AppComponent, 从而得到用 Dagger 管理的单例对象
+        mAppComponent = ArtUtils.obtainAppComponentFromContext(itemView.getContext());
+        mImageLoader = mAppComponent.imageLoader();
+    }
+
+    @Override
+    public void setData(@NonNull User data, int position) {
+        mName.setText(data.getLogin());
+
+        //itemView 的 Context 就是 Activity, Glide 会自动处理并和该 Activity 的生命周期绑定
+        mImageLoader.loadImage(itemView.getContext(),
+                ImageConfigImpl
+                        .builder()
+                        .url(data.getAvatarUrl())
+                        .imageView(mAvatar)
+                        .build());
+    }
+
+    /**
+     * 在 Activity 的 onDestroy 中使用 {@link DefaultAdapter# releaseAllHolder(RecyclerView)} 方法 (super.onDestroy() 之前)
+     * {@link BaseHolder#onRelease()} 才会被调用, 可以在此方法中释放一些资源
+     */
+    @Override
+    protected void onRelease() {
+        //只要传入的 Context 为 Activity, Glide 就会自己做好生命周期的管理, 其实在上面的代码中传入的 Context 就是 Activity
+        //所以在 onRelease 方法中不做 clear 也是可以的, 但是在这里想展示一下 clear 的用法
+        mImageLoader.clear(mAppComponent.application(), ImageConfigImpl.builder()
+                .imageViews(mAvatar)
+                .build());
+        this.mAvatar = null;
+        this.mName = null;
+        this.mAppComponent = null;
+        this.mImageLoader = null;
+    }
+}
